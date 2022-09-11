3fcd9a1718a193d1eb0c9ce739fe7456448d157e
Steven Brown <steven.brown@bliztek.com>
2022-09-11 07:56:34 -0400

    AWS Fix

--- nextjs/lib/aws/sesClient.ts
+++ nextjs/lib/aws/sesClient.ts
@@ -1,6 +1,8 @@
 import { SESClient } from "@aws-sdk/client-ses";
+
 // Set the AWS Region.
 const REGION = "us-east-1";
+
 // Create SES service object.
-const sesClient = new SESClient({ region: REGION });
+const sesClient = new SESClient({ region: REGION, credentials: { accessKeyId: process.env.AZM_ACCESS_KEY_ID ?? '', secretAccessKey:process.env.AZM_SECRET_ACCESS_KEY ??''} });
 export { sesClient };
